aff9c9d91624199fc5273942f0f88b80c5b50b51
Zhen Qian <xhdp123@126.com>
2018-07-05 01:11:04 +0800

    apply webkitView into chegg view instead of uiwebview

--- CheggUnlocker/tab_Bar_View/controller/ViewController.swift
+++ CheggUnlocker/tab_Bar_View/controller/ViewController.swift
@@ -5,13 +5,20 @@
 //  Created by Zhen Qian on 6/30/18.
 //  Copyright © 2018 Zhen Qian. All rights reserved.
 //
-
+import Alamofire
 import UIKit
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+       // Alamofire.request(URL:"http://localhost:6800/schedule.json", method:.post, parameters:chegg)
+        
+      
+      
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
